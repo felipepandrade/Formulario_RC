92bff080d6ec694568cdacf3e23107c6df0feada
felipepandrade <felipeportoandrade@gmail.com>
2025-12-04 16:40:19 -0300

    feat: Migrate backend to Vercel Serverless Functions
    Removes Express and Multer dependencies, refactoring the submission logic to use Vercel Serverless Functions. This prepares the application for deployment on Vercel, streamlining dependencies and infrastructure.

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy /api requests to your local Express server during development
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
 });
